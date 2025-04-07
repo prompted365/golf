2a1f4c7d5a971d3e916c032d25d09c28a5c19c12
aschlean <antoni@gmitruk.com>
2025-04-06 18:00:58 -0700

    fix[credentials] updated init

--- credentials/__init__.py
+++ credentials/__init__.py
@@ -1,18 +1,17 @@
 from .base import CredentialResolver
-from .models import (
-    Credential,
-    CredentialRequest,
-    CredentialType,
-    ResolvedCredential,
-)
+from .models import Credential, CredentialRequest, CredentialResult
+from .default import EnvCredentialResolver, EnvCredentialConfig
 
 __all__ = [
     # Base interfaces
     "CredentialResolver",
     
+    # Default implementation
+    "EnvCredentialResolver",
+    "EnvCredentialConfig",
+    
     # Models
     "Credential",
     "CredentialRequest",
-    "CredentialType",
-    "ResolvedCredential",
+    "CredentialResult",
 ]
