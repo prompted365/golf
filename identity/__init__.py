2cdc1665e7b4e6883db82dd7d05e809e5408f11d
aschlean <antoni@gmitruk.com>
2025-04-07 11:47:29 -0700

    refactor[identity] remove unused default identity resolver imports

--- identity/__init__.py
+++ identity/__init__.py
@@ -1,14 +1,9 @@
 from .base import IdentityResolver
 from .models import AgentIdentity
-from .default import AuthedIdentityResolver, AuthedConfig
 
 __all__ = [
     # Base interfaces
     "IdentityResolver",
-    
-    # Default implementation
-    "AuthedIdentityResolver",
-    "AuthedConfig",
     
     # Models
     "AgentIdentity",
