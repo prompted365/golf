6718ea4f66dc13b17fb34cc33cebbe3a6a70bce5
aschlean <antoni@gmitruk.com>
2025-04-12 15:45:13 -0700

    refactor[permissions] rename classes imports

--- permissions/engine/__init__.py
+++ permissions/engine/__init__.py
@@ -1,9 +1,9 @@
 """Engine module for evaluating permission policies."""
 
 from .opa_client import OPAClient
-from .policy_generator import RegoPolicyGenerator
+from .policy_generator import RegoGenerator
 
 __all__ = [
     "OPAClient",
-    "RegoPolicyGenerator"
+    "RegoGenerator"
 ] 
